d7803278917944456fff2ebbf70ec6e50e444fd2
carter <carter@gamerzone.co.uk>
2022-04-24 23:14:35 -0600

    boilerplate complete

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 import dotenv from 'dotenv';
 import { Client, Intents, Message } from 'discord.js';
+import fs from 'fs';
+import ICommand from './models/ICommand';
 
 dotenv.config();
 const PREFIX = '!';
+const commands: CommandDefinition[] = [];
+
+interface CommandDefinition {
+	name: string;
+	command: any;
+}
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
+const commandFiles = fs
+	.readdirSync('./src/commands/')
+	.filter((file) => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`).default;
+	const instance = new command() as ICommand;
+
+	commands.push({ name: instance.name, command });
+}
 
 client.on('messageCreate', async (msg: Message) => {
 	if (msg.author.bot) return;
 	if (!msg.content.startsWith(PREFIX)) return;
 
 	const args = msg.content.split(' ');
-	const command = args.shift()?.slice(PREFIX.length);
+	const userCommand = args.shift()?.slice(PREFIX.length);
+
+	const cmdDefinition = commands.find((c) => c.name === userCommand);
+	if (cmdDefinition) {
+		const x = new cmdDefinition.command();
+		await x.execute(msg, args);
+	}
 });
 
 client.once('ready', () => console.log('Bot ready.'));

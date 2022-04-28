a10370a89e1a7edfffd41303a6ad72e6e7350aca
Carter <carter@bruh.com>
2022-04-27 21:38:50 -0600

    translated to new framework

--- src/controllers/CatFactController.ts
+++ src/controllers/CatFactController.ts
@@ -1,17 +1,20 @@
 import { Message } from 'discord.js';
 import axios from 'axios';
+import BaseController from '../models/BaseController';
+import command from '../decorators/command';
 
-export default class CatFactController {
+export default class CatFactController extends BaseController {
 	/**
 	 * Gets a random catfact and prints it to the
 	 * discord chat.
 	 *
 	 * @param msg Discord.js Message object
 	 */
-	public async execute(msg: Message): Promise<void> {
+	@command('catfact')
+	public async handleCatFact(): Promise<void> {
 		const {
 			data: { fact },
 		} = await axios.get('https://catfact.ninja/fact');
-		await msg.reply(fact);
+		await this.message.reply(fact);
 	}
 }

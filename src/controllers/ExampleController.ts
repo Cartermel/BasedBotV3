8723725a624cc4047253a3ad027c6933be4ee1c5
Carter <carter@bruh.com>
2022-06-08 21:43:16 -0600

    feat: new example

--- src/controllers/ExampleController.ts
+++ src/controllers/ExampleController.ts
@@ -1,17 +1,32 @@
-import command from '../decorators/command';
-import BaseController from '../models/BaseController';
+import { Message } from 'discord.js';
+import { command, controller } from '../infrastructure/injection';
+import { injectable } from 'tsyringe';
+
+/** Example service that will be used in the controller */
+export class ExampleService {
+	exampleServiceMethod() {
+		return 'pong!';
+	}
+}
 
 /**
  * Example class showcasing implicit commands and BaseController functionality
  */
-export default class ExampleController extends BaseController {
+@controller()
+@injectable()
+export class ExampleController {
+	/** controller constructor demonstrating tsyringe dependency injection.
+	 * dependencies are automatically resolved at runtime. We never have to reference the constructor.
+	 * this is optional, we could forgo the @injectable annotation and constructor altogether
+	 */
+	constructor(private exampleService: ExampleService) {}
+
 	/**
 	 * Replies with pong to the caller
 	 */
 	@command('ping')
-	public async handlePing() {
-		// message is a property of BaseController
-		this.message.reply('pong');
+	public async handlePing(message: Message) {
+		await message.reply(this.exampleService.exampleServiceMethod());
 	}
 
 	/**
@@ -21,7 +36,15 @@ export default class ExampleController extends BaseController {
 	 * @param args
 	 */
 	@command('params')
-	public async handleParams(args: string[]) {
-		this.message.reply(args.join('-'));
+	public async handleParams(message: Message, args: string[]) {
+		await message.reply(args.join('-'));
+	}
+
+	/** This method takes no params, but is still invoked just fine
+	 * it can be invoked from discord.
+	 */
+	@command('noparams')
+	public async noParams() {
+		console.log('hello!');
 	}
 }
